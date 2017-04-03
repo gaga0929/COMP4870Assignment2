1ad5138be5a2385881bd4eda53274c165fcc51ab
Janice Chan <gaga.0929@gmail.com>
2017-04-02 23:17:21 -0700

    commit before submission

--- src/ZenithClientSite/src/app/event-detail/event-detail.component.ts
+++ src/ZenithClientSite/src/app/event-detail/event-detail.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Event} from '../Event'
+import {Activity} from '../Activity'
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import { EventService } from '../event.service';
+import { ActivityService } from '../activity.service';
 
 
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.component.html',
-  styleUrls: ['./event-detail.component.css']
+  styleUrls: ['./event-detail.component.css'],
+  providers: [EventService, ActivityService]
 })
 export class EventDetailComponent implements OnInit {
 
@@ -16,6 +19,7 @@ export class EventDetailComponent implements OnInit {
 
   constructor(
   private eventService: EventService,
+  private activityService: ActivityService,
   private route: ActivatedRoute,
   private location: Location
   ) { }
@@ -26,11 +30,27 @@ export class EventDetailComponent implements OnInit {
     this.eventService.getEventById(id)
       .then(result => this.selectedEvent = result);
   });
+  this.getActivities();
 }
 
 
+  activities: Activity[] = [];
+
+  getActivities(): void {
+      this.activityService.getActivities()
+    .then(activities => this.activities = activities);
+  }
+
   @Input()
   selectedEvent: Event;
+  
+  selectedActivity: Activity;
+
+
+  onChange(val) {
+    this.selectedActivity = JSON.parse(val);
+    this.selectedEvent.activityDetails.description = this.selectedActivity.description
+  }
 
   goBack(): void {
   this.location.back();
